4ddf71b3f3af7bf91c841d0625da79a5c3fc4a52
giladkovalevski <kovalevskigilad@gmail.com>
2021-02-21 14:30:56 -0500

    updated

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const email = "admin@yopmail.com";
 const password = "adminyopmail";
 
+const message = document.getElementById("message-date-time-container");
+message.innerHTML = moment().format('LL');;
+
 function showWeather() {
+
     const xhrObj = new XMLHttpRequest();
 
     const url = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/56186?apikey=KGVMhb1HTYOEl28HUkA0sCcGstJBWrK6&metric=true";
@@ -9,13 +13,12 @@ function showWeather() {
     xhrObj.open("GET", url);
 
     xhrObj.onload = displayWeather;
-    xhrObj.onerror = errMessage;
 
     xhrObj.send();
 }
-// my weather app
 
 function displayWeather(resp) {
+
     const userEmail = document.getElementById("user-email");
     const userEmailValue = userEmail.value;
 
@@ -31,40 +34,98 @@ function displayWeather(resp) {
         for (let i = 0; i < dailyForecast.length; i++) {
 
             const date = dailyForecast[i].Date;
+            const dateDiv = document.createElement("span");
+            dateDiv.innerHTML = `\n${date}`;
+            dateDiv.style.color = "blue";
+            dateDiv.style.textDecoration = "underline";
+            console.log(dateDiv);
             const maxTemp = dailyForecast[i].Temperature.Maximum.Value;
             const minTemp = dailyForecast[i].Temperature.Minimum.Value;
             const day = dailyForecast[i].Day.IconPhrase;
             const night = dailyForecast[i].Night.IconPhrase;
 
-            const onDisplay = `${date}
-    Max: ${maxTemp}C Min: ${minTemp}C
+            const onDisplay = `Max: ${maxTemp}C Min: ${minTemp}C
     Day: ${day} Night: ${night}`;
 
             let weatherContainer = document.getElementById("weather-container");
-            let newDivEl = document.createElement("div");
-
+            let newDivEl = document.createElement("p");
+            newDivEl.style.margin = "0";
+            weatherContainer.appendChild(dateDiv);
             newDivEl.innerText = onDisplay;
             weatherContainer.appendChild(newDivEl);
         }
-    } else {
+    }
+}
+
+function submitHandler(event) {
+    
+    event.preventDefault();
+    const userEmail = document.getElementById("user-email");
+    const userEmailValue = userEmail.value;
+
+    const userPassword = document.getElementById("user-password");
+    const userPasswordValue = userPassword.value;
+
+
+
+    if (userEmailValue === "" && userPasswordValue.length < 6) {
+        let errorsContainer = document.getElementById("errors");
+        let newDivErrEl = document.createElement("div");
         const errMessage = `Error! Please complete the 
         form!
         * Email address must be filled in!
         * Password length must be at
         least 6 characters!`;
 
-        let weatherContainer = document.getElementById("weather-container");
+        newDivErrEl.style.color = "red";
+        newDivErrEl.innerText = errMessage;
+        errorsContainer.appendChild(newDivErrEl);
+        // newDivErrEl.style.color = "red";
+        // newDivErrEl.innerText = `* Email address must be filled in!`;
+
+        // weatherContainer.appendChild(newDivErrEl);
+
+    } else if (userEmailValue === "" && userPasswordValue.length >= 6) {
+        let errorsContainer = document.getElementById("errors");
         let newDivErrEl = document.createElement("div");
+        const errMessage = `Error! Please complete the 
+        form!
+        * Email address must be filled in!
+        `;
 
         newDivErrEl.style.color = "red";
         newDivErrEl.innerText = errMessage;
-        weatherContainer.appendChild(newErrDivEl);
+        errorsContainer.appendChild(newDivErrEl);
+
+    } else if (userEmailValue !== "" && userPasswordValue.length < 6) {
+        let errorsContainer = document.getElementById("errors");
+        let newDivErrEl = document.createElement("div");
+        const errMessage = `Error! Please complete the 
+        form!
+        * Password length must be at
+        least 6 characters!`;
+
+        newDivErrEl.style.color = "red";
+        newDivErrEl.innerText = errMessage;
+        errorsContainer.appendChild(newDivErrEl);
+    } else if (userEmailValue !== email && userPasswordValue !== password) {
+        let errorsContainer = document.getElementById("errors");
+        let newDivErrEl = document.createElement("div");
+        const errMessage = `Error! Please complete the 
+        form!
+        * Wrong email or password`;
+
+        newDivErrEl.style.color = "red";
+        newDivErrEl.innerText = errMessage;
+        errorsContainer.appendChild(newDivErrEl);
     }
-}
+    else {
+        let errorsContainer = document.getElementById("errors");
+        errorsContainer.style.display = "none";
+        showWeather();
+    }
 
-function errMessage() {
-
-
+    console.log(userEmailValue + userPasswordValue);
 }
 
 
